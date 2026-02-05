190db8aaccd08b1e84d3fc201a106b60b87ffde6
S-P-A-C-E-B-A-R <72162631+S-P-A-C-E-B-A-R@users.noreply.github.com>
2026-02-05 18:34:09 -0500

    Fixing -Wreorder
    Cleaning up a minor -Wreorder compiler message.

--- src/LiteMenu.cpp
+++ src/LiteMenu.cpp
@@ -1,14 +1,23 @@
 #include "LiteMenu.h"
 
+// Static member definition - shared across all Menu instances
+Menu* Menu::activeMenu = nullptr;
+
 // Constructor: Initializes the Menu object with a heading and looping behavior.
 // The active menu and selection are set to the current menu and the first item respectively.
 Menu::Menu(const std::string& heading, bool loop)
-    : heading(heading),               // Set the heading of the menu.
-      activeSelect(0),                // Initialize active selection to the first item.
-      previousSelect(0),              // Initialize previous selection to the first item.
-      previousMenu(nullptr),          // Previous menu is initially set to null.
-      behaveLoop(loop),               // Set the looping behavior (true/false).
-      activeMenu(this) {}             // The active menu is the current menu instance.
+        : heading(heading),               // Set the heading of the menu.
+            behaveLoop(loop),               // Set the looping behavior (true/false).
+            subMenus(),                      // Ensure sub-menus vector is default-initialized.
+            entries(),                       // Ensure entries vector is default-initialized.
+            activeSelect(0),                // Initialize active selection to the first item.
+            previousMenu(nullptr),          // Previous menu is initially set to null.
+            previousSelect(0) {             // Initialize previous selection to the first item.
+    // If activeMenu is not set, initialize it to this menu (root menu)
+    if (activeMenu == nullptr) {
+        activeMenu = this;
+    }
+}
 
 // Destructor: Cleans up any sub-menus created by this menu.
 // This is necessary to avoid memory leaks, as sub-menus are dynamically allocated.
@@ -41,11 +50,14 @@ size_t Menu::getactiveSelect() const {
 // - 'down' moves the selection down, looping if necessary.
 // - 'enter' triggers the action associated with the currently selected menu item.
 void Menu::navigate(HMI navigation) {
+    // Always operate on the currently active menu
+    Menu* currentMenu = activeMenu;
+    
     switch (navigation) {
         case HMI::enter:
             // If the selected item has an action, execute it. Otherwise, print "No Action Assigned".
-            if (activeMenu->entries[activeMenu->activeSelect].action) {
-                activeMenu->entries[activeMenu->activeSelect].action();
+            if (currentMenu->entries[currentMenu->activeSelect].action) {
+                currentMenu->entries[currentMenu->activeSelect].action();
             } else {
                 std::cout << "No Action Assigned" << std::endl;
             }
@@ -53,19 +65,19 @@ void Menu::navigate(HMI navigation) {
 
         case HMI::up:
             // Move selection up, if possible. If at the top and looping is enabled, wrap to the bottom.
-            if (activeMenu->activeSelect > 0) {
-                --activeMenu->activeSelect;
-            } else if (activeMenu->behaveLoop) {
-                activeMenu->activeSelect = activeMenu->entries.size() - 1;
+            if (currentMenu->activeSelect > 0) {
+                --currentMenu->activeSelect;
+            } else if (currentMenu->behaveLoop) {
+                currentMenu->activeSelect = currentMenu->entries.size() - 1;
             }
             break;
 
         case HMI::down:
             // Move selection down, if possible. If at the bottom and looping is enabled, wrap to the top.
-            if (activeMenu->activeSelect < activeMenu->entries.size() - 1) {
-                ++activeMenu->activeSelect;
-            } else if (activeMenu->behaveLoop) {
-                activeMenu->activeSelect = 0;
+            if (currentMenu->activeSelect < currentMenu->entries.size() - 1) {
+                ++currentMenu->activeSelect;
+            } else if (currentMenu->behaveLoop) {
+                currentMenu->activeSelect = 0;
             }
             break;
     }
@@ -82,26 +94,35 @@ void Menu::AddMenuItem(const std::string& title,
                       bool visible,
                       bool state,
                       std::function<void()> action) {
+    // Always add to the currently active menu, not the base menu
     this->entries.emplace_back(title, submenu, visible, state, action);
 }
 
 // AddSubMenu: Adds a sub-menu to the current menu.
 // A "Back" option is automatically added to return to the previous menu.
 void Menu::AddSubMenu(Menu* submenu) {
-    submenu->previousMenu = activeMenu;  // Set the previous menu of the sub-menu to the current menu.
+    // Store the current menu that will be the parent - use 'this' when called on a specific menu
+    Menu* parentMenu = this;
+    
+    submenu->previousMenu = parentMenu;  // Set the previous menu of the sub-menu to the current menu.
 
     // Add a "Back" menu item to return to the previous menu when selected.
     submenu->AddMenuItem("Back", false, true, false, [this, submenu]() {
         activeMenu = submenu->previousMenu;
+        // Restore the previous selection index when going back
+        activeMenu->activeSelect = submenu->previousSelect;
     });
 
-    // Add the sub-menu as a selectable item in the current menu.
-    AddMenuItem(submenu->getHeading(), true, true, false, [this, submenu]() {
+    // Add the sub-menu as a selectable item in the parent menu.
+    parentMenu->AddMenuItem(submenu->getHeading(), true, true, false, [this, submenu, parentMenu]() {
+        submenu->previousMenu = parentMenu;  // Ensure we know where we came from
+        submenu->previousSelect = parentMenu->activeSelect;  // Save current selection
         activeMenu = submenu;
+        submenu->activeSelect = 0;  // Reset selection to first item when entering submenu
     });
 
     // Store the sub-menu in the list of sub-menus for deletion in the destructor.
-    activeMenu->subMenus.emplace_back(submenu);
+    parentMenu->subMenus.emplace_back(submenu);
 }
 
 // updateMenuItem: Updates an existing menu item based on its index.
